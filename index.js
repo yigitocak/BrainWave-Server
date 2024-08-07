ed3db79d276209b8e85fffb9e6592d2a013357ee
yigit ocak <yigitockk@gmail.com>
2024-08-07 00:10:41 -0400

    Add login route and JSON body parser middleware to index.js

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ import morgan from "morgan";
 import "dotenv/config";
 import signup from "./routes/signup.js";
 import resume from "./routes/resume.js";
+import login from "./routes/login.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(morgan("dev"));
 app.use(cors());
+app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/signup", signup);
 app.use("/resume", resume);
+app.use("/login", login);
 
 app.listen(PORT || 5050, () => {
   console.log(`Running on: http://localhost:${PORT}`);
