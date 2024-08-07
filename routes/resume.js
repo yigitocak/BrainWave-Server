6fe46f2fcc1830d38b1f3ac0da98c9b5d3d1f9db
yigit ocak <yigitockk@gmail.com>
2024-08-07 00:11:59 -0400

    remove JSON body parser

--- routes/resume.js
+++ routes/resume.js
@@ -2,8 +2,6 @@ import express from "express";
 import { downloadResume } from "../views/resumeViews.js";
 const resume = express.Router();
 
-resume.use(express.json());
-
 resume.get("/", downloadResume);
 
 export default resume;
--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,7 @@
-import express from "express"
-const signup = express.Router()
-import {signUpUser} from "../views/signupViews.js"
+import express from "express";
+const signup = express.Router();
+import { signUpUser } from "../views/signupViews.js";
 
-signup.use(express.json())
+signup.post("/", signUpUser);
 
-signup.post("/", signUpUser)
-
-export default signup
+export default signup;
