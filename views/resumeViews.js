0446b9971194df04b8bd4aa5732a6fe07c3ac159
yigit ocak <yigitockk@gmail.com>
2024-08-07 00:12:15 -0400

    better error handling

--- views/resumeViews.js
+++ views/resumeViews.js
@@ -1,12 +1,18 @@
 import path from "path";
 
 export const downloadResume = async (_req, res) => {
-  const filePath = path.resolve("files/resume.pdf");
-  res.download(filePath, "resume.pdf", (err) => {
-    if (err) {
-      res
-        .status(500)
-        .json({ message: "Failed to download file", success: false });
-    }
-  });
+  try {
+    const filePath = path.resolve("files/resume.pdf");
+    res.download(filePath, "resume.pdf", (err) => {
+      if (err) {
+        console.error("Failed to download file:", err);
+        res
+          .status(500)
+          .json({ message: "Failed to download file", success: false });
+      }
+    });
+  } catch (error) {
+    console.error("Internal Server Error:", error);
+    res.status(500).json({ message: "Internal Server Error", success: false });
+  }
 };
