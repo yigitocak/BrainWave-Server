1b58e7955fad04d45d1f095c3ecbb3b6bb135d1c
yigit ocak <yigitockk@gmail.com>
2024-08-07 14:22:30 -0400

    add auth endpoint and change view name

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,10 @@
 import express from "express";
 const login = express.Router();
-import { loginViews } from "../views/loginViews.js";
 
-login.post("/", loginViews);
+import { loginUserView, decodeView } from "../views/loginViews.js";
+import authenticateToken from "../middlewares/authenticateToken.js";
+
+login.post("/", loginUserView);
+login.get("/auth", authenticateToken, decodeView);
 
 export default login;
