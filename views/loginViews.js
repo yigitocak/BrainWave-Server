1775ee2c8919075be53284f2d00eb152be2b6a02
yigit ocak <yigitockk@gmail.com>
2024-08-07 14:21:33 -0400

    use helper function to create and set token when signup/login

--- views/loginViews.js
+++ views/loginViews.js
@@ -1,66 +1,57 @@
-import jwt from "jsonwebtoken";
 import knex from "knex";
-import knexFile from "../knexfile.js";
+import knexfile from "../knexfile.js";
 import bcrypt from "bcrypt";
-const db = knex(knexFile.development);
+const db = knex(knexfile.development);
+import { generateTokenAndSetCookie } from "./helpers/token.js";
+import "dotenv/config";
 
-export const loginViews = async (req, res) => {
+export const loginUserView = async (req, res) => {
   const { email, password, rememberMe } = req.body;
+
   if (!email || !password) {
     return res.status(400).json({
-      message: "Bad request body! Email and password are required.",
       success: false,
+      message: "Bad request body.",
     });
   }
 
   try {
     const user = await db("users").where({ email }).first();
     if (!user) {
-      return res
-        .status(404)
-        .json({ message: "User not found!", success: false });
-    }
-
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
-      return res
-        .status(401)
-        .json({ message: "Password doesn't match", success: false });
-    }
-
-    const SECRET_KEY = process.env.SECRET_KEY;
-    if (!SECRET_KEY) {
-      return res.status(500).json({
-        message: "Internal server error. SECRET_KEY is not defined.",
+      return res.status(404).json({
+        message: "User not found",
         success: false,
       });
     }
 
-    const expiresIn = rememberMe ? "7d" : "12h";
-    const token = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-      },
-      SECRET_KEY,
-      { expiresIn },
-    );
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(401).json({
+        message: "Invalid Login",
+        success: false,
+      });
+    }
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
-      maxAge: rememberMe ? 7 * 24 * 60 * 60 * 1000 : 12 * 60 * 60 * 1000,
-    });
+    generateTokenAndSetCookie(user, rememberMe, res);
+    console.log(user.name);
 
     return res.status(200).json({
       success: true,
+      tokenProvided: true,
+      userName: user.name,
       message: rememberMe ? "Logged in for 7 days" : "Logged in for 12 hours",
     });
   } catch (e) {
-    console.error("Internal Error when logging user:", e);
-    return res
-      .status(500)
-      .json({ message: "Internal Server Error", success: false });
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
   }
+};
+
+export const decodeView = (req, res) => {
+  return res.status(200).json({
+    message: "Login Successful",
+    success: true,
+  });
 };
--- views/signupViews.js
+++ views/signupViews.js
@@ -2,17 +2,16 @@ import knex from "knex";
 import knexFile from "../knexfile.js";
 import bcrypt from "bcrypt";
 const db = knex(knexFile.development);
+import { generateTokenAndSetCookie } from "./helpers/token.js";
 
 export const signUpUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, rememberMe = false } = req.body;
 
   if (!name || !email || !password) {
-    return res
-      .status(400)
-      .json({
-        message: "Bad request body! Name, email, and password are required.",
-        success: false,
-      });
+    return res.status(400).json({
+      message: "Bad request body! Name, email, and password are required.",
+      success: false,
+    });
   }
 
   const emailRegex = /\S+@\S+\.\S+/;
@@ -40,11 +39,13 @@ export const signUpUser = async (req, res) => {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    await db("users").insert({
+    const user = await db("users").insert({
       email,
       name,
       password: hashedPassword,
     });
+
+    generateTokenAndSetCookie(user, rememberMe, res);
 
     return res
       .status(201)
