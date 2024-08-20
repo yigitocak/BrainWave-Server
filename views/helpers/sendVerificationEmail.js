402b000c18e946124249f4b3d148fcc1f878a7ae
yigit ocak <yigitockk@gmail.com>
2024-08-20 02:48:25 -0400

    create sendVerificationEmail helper function

---
+++ views/helpers/sendVerificationEmail.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+
+export const sendVerificationEmail = async (email, verificationCode) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "7a7d6a003@smtp-brevo.com",
+        pass: "O4UEj5b9XDB2rRs6",
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Email Verification",
+      text: `Your verification code is: ${verificationCode}`,
+      html: `<p>Your verification code is: <strong>${verificationCode}</strong></p>`,
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log("Verification email sent to:", email);
+  } catch (error) {
+    console.error("Error sending verification email:", error);
+    throw new Error("Could not send verification email.");
+  }
+};
