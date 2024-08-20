4fea38b64ef79aeea98bb448b9027969472d8384
yigit ocak <yigitockk@gmail.com>
2024-08-20 02:09:16 -0400

    add title to questions

--- models/Challenges.js
+++ models/Challenges.js
@@ -1,10 +1,11 @@
 import { Schema, model } from "mongoose";
 
 const challengesSchema = new Schema({
-  questionId: { type: String, required: true },
+  questionId: { type: String, required: true, unique: true },
   question: { type: String, required: true },
-  answer: { type: String, required: true },
+  difficulty: { type: String, required: true },
   peopleSolved: { type: Number, required: true },
+  title: { type: String, required: true },
 });
 
 const Challenges = model("Challenges", challengesSchema);
