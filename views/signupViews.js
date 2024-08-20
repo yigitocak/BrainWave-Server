f52e0aba1f3ae843f2953a0b0dd4b23b985bedb2
yigit ocak <yigitockk@gmail.com>
2024-08-20 02:45:59 -0400

    update signupViews to handle verification

--- views/signupViews.js
+++ views/signupViews.js
@@ -1,9 +1,10 @@
 import Users from "../models/Users.js";
 import bcrypt from "bcrypt";
-import { generateTokenAndSetCookie } from "./helpers/token.js";
+import { sendVerificationEmail } from "./helpers/sendVerificationEmail.js";
+import crypto from "crypto";
 
 export const signUpUser = async (req, res) => {
-  const { name, email, password, rememberMe = false } = req.body;
+  const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     return res.status(400).json({
@@ -37,17 +38,30 @@ export const signUpUser = async (req, res) => {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
+    const verificationCode = crypto
+      .randomBytes(3)
+      .toString("hex")
+      .toUpperCase();
+    const verificationCodeExpires = Date.now() + 3600000;
+
     const user = await Users.create({
       name,
       email,
       password: hashedPassword,
+      verificationCode,
+      verificationCodeExpires,
+      solvedQuestions: [],
+      onGoingQuestions: [],
+      threads: [],
     });
 
-    generateTokenAndSetCookie(user, rememberMe, res);
+    await sendVerificationEmail(email, verificationCode);
 
-    return res
-      .status(201)
-      .json({ message: "Account created successfully!", success: true });
+    return res.status(201).json({
+      message: "Account created successfully! Please verify your email.",
+      success: true,
+      needsVerification: true,
+    });
   } catch (e) {
     console.error("Internal Error when creating user:", e);
     return res
